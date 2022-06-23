1b59d163c5da935c43750de145dd066081000f88
ekkyarmandi <ekkyarmandi@gmail.com>
2022-06-23 11:17:50 +0700

    remove hidden table

--- static/js/requestEarnings.js
+++ static/js/requestEarnings.js
@@ -4,7 +4,6 @@ const getEarnings = () => {
     xhr.onload = function () {
         if (xhr.status == 200) {
             let entries = JSON.parse(this.response);
-            let table = document.getElementById("earning-table");
             let earning_usd = document.getElementById("usd");
             let earning_idr = document.getElementById("idr");
             let usd_idr = document.getElementById("usd_idr_rate");
@@ -13,7 +12,6 @@ const getEarnings = () => {
             earning_idr.innerHTML = entries["value_idr"];
             usd_idr.innerHTML = entries["usd_idr"];
             avail_date.innerHTML = entries["avail_date"];
-            table.removeAttribute("hidden");
         }
     };
     xhr.send();
